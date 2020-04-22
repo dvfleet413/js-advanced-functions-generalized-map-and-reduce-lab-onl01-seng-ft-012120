0734fda7928f9047cc341593ba06c15c81b0c760
github username <github email address>
2020-04-21 20:47:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+const map = function(array, fn){
+    const result = [];
+    for(let i = 0; i < array.length; i++){
+        result.push(fn(array[i]));
+    };
+    return result;
+};
+
+
+// result needs to start as a truthy value when checking that all values are truthy
+// but needs to start as 0 when doing math
+// if nothing is passed in for init //=> falsely
+const reduce = function(array, fn, init){
+    let result = init;
+    for(let i = 0; i < array.length; i++){
+        if (result === undefined){
+            result = array[i];
+        }
+        else {
+            result = fn(array[i], result);
+        }
+    };
+    return result;
+};
